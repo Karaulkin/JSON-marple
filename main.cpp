c52084af4de17423db3376c29c5df29cd0bca965
Kirill <katinaka@student.21-school.ru>
2024-04-09 15:01:42 +0300

    example

--- main.cpp
+++ main.cpp
@@ -7,10 +7,13 @@
 
 int main() {
     std::string NameJson = "example.json";
+    std::string NameTxt = "example.txt";
 
     // открыть поток для чтения
+    std::ofstream fille_json(NameJson);
+    std::ifstream fille_txt(NameTxt);
     // открыть поток для записи
-    ParserJson p(NameJson);
+    ParserJson p(&fille_json, &fille_txt);
     // ... Передать ссылку на потоки
     p.Parse();
     // закрыть поток для записи
